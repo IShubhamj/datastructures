3d6c5cc7313f74c64cc90fe059b94b8c7e3727c7
Shubham Joshi <shubham.joshi2812@gmail.com>
2021-05-24 14:32:09 +0530

    feat: Added Solution for Capitalize

---
+++ Capitalize/Solution1.js
@@ -0,0 +1,8 @@
+function capitalize(str) {
+  return str
+    .split(" ")
+    .map((word) => word[0].toUpperCase() + word.slice(1))
+    .join(" ");
+}
+
+console.log(capitalize("ace in algorithm interviews"));
